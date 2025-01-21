89d7ef062bd0bb76c922ab9405d5b5339e3cdb33
GHADAHIOS <ghada515alenzi515@gmail.com>
2025-01-21 16:52:45 +0300

    Initial Commit

---
+++ moviesApp/moviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviesAppApp.swift
+//  moviesApp
+//
+//  Created by GHADAH ALENEZI on 21/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct moviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ moviesAppUITests/moviesAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  moviesAppUITestsLaunchTests.swift
+//  moviesAppUITests
+//
+//  Created by GHADAH ALENEZI on 21/07/1446 AH.
+//
+
+import XCTest
+
+final class moviesAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
